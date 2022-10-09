d8af0bfeb9465d30b4b71d81d77de3366a1588d0
shes1996 <108803563+shes1996@users.noreply.github.com>
2022-10-09 16:06:36 +0400

    The third solution
    solution with dicts and poses

--- main.py
+++ main.py
@@ -45,22 +45,20 @@ power = int(input('Введите натуральную степень мног
 
 
 def polinom(power):
-    with open('file1.txt', 'w') as polinom:
+    with open('file2.txt', 'w') as polinom:
         for i in range(power, -1, -1):
+            koef = randint(0, 100)
             if i > 0:
-                koef = randint(0, 100)
                 if koef > 0:
                     polinom.write(f'{str(koef)}x^{str(i)} + ')
-                else:
-                    continue
             else:
-                koef = randint(0, 100)
                 if koef > 0:
-                    polinom.write(f'{str(koef)} =0')
+                    polinom.write(f'{str(koef)} = 0')
                 else:
-                    polinom.write(' =0')
+                    polinom.write(' = 0')
 
-#print(polinom(power))
+#polinom(power)
+
 
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
 
@@ -69,6 +67,53 @@ with open('file1.txt', 'r') as polinom1:
 with open('file2.txt', 'r') as polinom2:
     my_str2 = polinom2.read()
 
+my_list1 = my_str1.split(' + ')
+my_list2 = my_str2.split(' + ')
+
+dictionary1 = {}
+
+for i in range(len(my_list1)):
+    if 'x' in my_list1[i]:
+        posX = my_list1[i].find('x')
+        dictionary1[int(my_list1[i][posX + 2:])] = int(my_list1[i][:posX])
+    else:
+        posEqualy = my_list1[i].find(' =')
+        dictionary1[0] = int(my_list1[i][:posEqualy])
+
+print(dictionary1)
+dictionary2 = {}
+
+for i in range(len(my_list2)):
+    if 'x' in my_list2[i]:
+        posX = my_list2[i].find('x')
+        dictionary2[int(my_list2[i][posX + 2:])] = int(my_list2[i][:posX])
+    else:
+        posEqualy = my_list2[i].find(' = ')
+        dictionary2[0] = int(my_list2[i][:posEqualy])
+
+print(dictionary2)
+
+maxDegree = max(max(dictionary1.keys()), max(dictionary2.keys()))
+
+with open('file3.txt', 'w') as two_polinom:
+    for i in range(maxDegree, 0, -1):
+        koef = 0
+        if i in dictionary1.keys():
+            koef += dictionary1[i]
+        if i in dictionary2.keys():
+            koef += dictionary2[i]
+        if koef != 0:
+            two_polinom.write(f'{koef}x^{i} + ')
+    koef = 0
+    if 0 in dictionary1.keys():
+        koef += dictionary1[0]
+    if 0 in dictionary2.keys():
+         koef += dictionary2[0]
+    if koef != 0:
+         two_polinom.write(f'{koef} = 0')
+
+
+'''
 my_list1 = my_str1.split(' + ')
 my_list2 = my_str2.split(' + ')
 print(my_list1)
@@ -113,3 +158,4 @@ with open('file3.txt', 'w') as two_polinom:
             two_polinom.write(f'{str(koefs[i])}x + ')
         else:
             two_polinom.write(f'{str(koefs[i])} = 0')
+'''
