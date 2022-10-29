eec950eeffff56a1e5de68b4ec140501666d2d0c
shes1996 <108803563+shes1996@users.noreply.github.com>
2022-10-29 19:08:50 +0400

    attempt to use lambda, zip, filter, map, list comprehension, enumerate

--- main.py
+++ main.py
@@ -1,48 +1,25 @@
-# Вычислить число c заданной точностью d
-# Пример - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
-from decimal import Decimal
-
-pi = '3.14159265358979323846'
-accuracy = lambda x: Decimal(x).quantize(Decimal(input('Введите точность d в виде 0.001: ')))
-print(accuracy(pi))
-
-# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-
-user_number = int(input('Insert your number: '))
-
-i = 2
-multipliers = []
-while i ** 2 <= user_number:
-    while user_number % i == 0:
-        multipliers.append(i)
-        user_number = user_number // i
-    i = i + 1
-if user_number > 1:
-    multipliers.append(user_number)
-print(multipliers)
+from random import randint
 
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
-nums = [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
-new_nums = []
-for i in range(len(nums)):
-    count = 0
-    for j in range(len(nums)):
-        if nums[i] == nums[j]:
-            count += 1
-    if count == 1:
-        new_nums.append(nums[i])
 
-print(nums)
-print(new_nums)
+nums = [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
+unique = []
+[unique.append(nums[i]) for i in range(len(nums)) if nums[i] not in unique]
+print(unique)
+print([nums[i] for i in range(len(nums)) if nums.count(nums[i]) == 1])
 
 # Задана натуральная степень k.
 # Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример: - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-from random import randint
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример: - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
 power = int(input('Введите натуральную степень многочлена (наивысшая степень в полиноме): '))
 
+def func(elem):
+    return elem > 0
 
 def polinom(power):
     with open('file2.txt', 'w') as polinom:
@@ -57,7 +34,7 @@ def polinom(power):
                 else:
                     polinom.write(' = 0')
 
-#polinom(power)
+# polinom(power)
 
 
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
@@ -70,6 +47,10 @@ with open('file2.txt', 'r') as polinom2:
 my_list1 = my_str1.split(' + ')
 my_list2 = my_str2.split(' + ')
 
+print(len(my_list1), len(my_list2))
+
+print(list(zip(my_list1, my_list2)))    # I don't know how use 'zip' and 'filter' in these tasks
+
 dictionary1 = {}
 
 for i in range(len(my_list1)):
@@ -77,7 +58,7 @@ for i in range(len(my_list1)):
         posX = my_list1[i].find('x')
         dictionary1[int(my_list1[i][posX + 2:])] = int(my_list1[i][:posX])
     else:
-        posEqualy = my_list1[i].find(' =')
+        posEqualy = my_list1[i].find('=')
         dictionary1[0] = int(my_list1[i][:posEqualy])
 
 print(dictionary1)
@@ -87,6 +68,7 @@ for i in range(len(my_list2)):
     if 'x' in my_list2[i]:
         posX = my_list2[i].find('x')
         dictionary2[int(my_list2[i][posX + 2:])] = int(my_list2[i][:posX])
+
     else:
         posEqualy = my_list2[i].find(' = ')
         dictionary2[0] = int(my_list2[i][:posEqualy])
@@ -98,10 +80,14 @@ maxDegree = max(max(dictionary1.keys()), max(dictionary2.keys()))
 with open('file3.txt', 'w') as two_polinom:
     for i in range(maxDegree, 0, -1):
         koef = 0
-        if i in dictionary1.keys():
-            koef += dictionary1[i]
-        if i in dictionary2.keys():
-            koef += dictionary2[i]
+        try:
+            if i in dictionary1.keys() or dictionary2.keys():
+                koef += list(map(lambda dictionary1, dictionary2: dictionary1[i] + dictionary2[i]))
+        except:
+            if i in dictionary1.keys():
+                koef += dictionary1[i]
+            if i in dictionary2.keys():
+                koef += dictionary2[i]
         if koef != 0:
             two_polinom.write(f'{koef}x^{i} + ')
     koef = 0
